cbb41801924b32f09e3db524aaa997ea67df543e
devliet <etienne.devlieger@gmail.com>
2024-08-30 09:50:43 -0400

    Module 3 capstone

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -19,6 +19,7 @@ from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
 
+
 urlpatterns = [
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
@@ -27,4 +28,5 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
+   
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
