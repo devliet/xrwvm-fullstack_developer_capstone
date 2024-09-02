dddd30bba63897e6348254fb514143b8516eeebc
devliet <etienne.devlieger@gmail.com>
2024-09-02 06:32:01 -0400

    models

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -13,7 +13,7 @@ from django.contrib.auth import login, authenticate
 import logging
 import json
 from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
+from .populate import initiate
 from .models import CarMake, CarModel
 
 # Get an instance of a logger
@@ -21,17 +21,6 @@ logger = logging.getLogger(__name__)
 
 
 # Create your views here.
-def get_cars(request):
-    count = CarMake.objects.filter().count()
-    print(count)
-    if(count == 0):
-        initiate()
-    car_models = CarModel.objects.select_related('car_make')
-    cars = []
-    for car_model in car_models:
-        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
-    return JsonResponse({"CarModels":cars})
-
     
 # Create a `login_request` view to handle sign in request
 @csrf_exempt
@@ -96,7 +85,18 @@ def registration(request):
     else :
         data = {"userName":username,"error":"Already Registered"}
         return JsonResponse(data)
-
+@csrf_exempt
+def get_cars(request):
+    logging.info("call get cars")
+    count = CarMake.objects.filter().count()
+    print(count)
+    if(count == 0):
+        initiate()
+    car_models = CarModel.objects.select_related('car_make')
+    cars = []
+    for car_model in car_models:
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 from django.conf import settings
+from djangoapp import views
 
 
 urlpatterns = [
@@ -28,5 +29,6 @@ urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
+    path('get_cars/', views.get_cars)
    
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
