e8205ec0a2499cfbe262f1389b8acfc9b9bc8786
devliet <101347493+devliet@users.noreply.github.com>
2024-09-05 15:16:32 +0200

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,13 +12,19 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_request, name='logout'),
     path(route='register', view=views.registration, name='registration'),
-    path(route='get_cars/', view=views.get_cars, name ='getcars'),
+    path(route='get_cars/', view=views.get_cars, name='getcars'),
 
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
 
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='get_dealers/<str:state>',
+         view=views.get_dealerships,
+         name='get_dealers_by_state'
+        ),
 
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>',
+         view=views.get_dealer_details,
+         name='dealer_details'
+        ),
     # path for dealer reviews view
 
     # path for add a review view
@@ -27,5 +33,5 @@ urlpatterns = [
          name='dealer_details'
         ),
     path(route='add_review', view=views.add_review, name='add_review'),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
