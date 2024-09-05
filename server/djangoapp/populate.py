563a1aeaac345c8d40cb70f9318d8e2e87cc89d1
devliet <101347493+devliet@users.noreply.github.com>
2024-09-05 13:15:50 +0200

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -23,20 +23,20 @@ def initiate():
              "car_make": car_make_instances[0]
       },
       {
-          "name": "Qashqai", "type": "SUV", "year": 2023,
-          "car_make": car_make_instances[0]
+              "name": "Qashqai", "type": "SUV", "year": 2023,
+              "car_make": car_make_instances[0]
       },
       {
-          "name": "XTRAIL", "type": "SUV", "year": 2023,
-          "car_make": car_make_instances[0]
+              "name": "XTRAIL", "type": "SUV", "year": 2023,
+              "car_make": car_make_instances[0]
       },
       {
-          "name": "A-Class", "type": "SUV", "year": 2023,
-          "car_make": car_make_instances[1]
+              "name": "A-Class", "type": "SUV", "year": 2023,
+              "car_make": car_make_instances[1]
       },
       {
-          "name": "C-Class", "type": "SUV", "year": 2023,
-        "car_make": car_make_instances[1]
+              "name": "C-Class", "type": "SUV", "year": 2023,
+              "car_make": car_make_instances[1]
       },
       {
           "name": "E-Class", "type": "SUV", "year": 2023,
