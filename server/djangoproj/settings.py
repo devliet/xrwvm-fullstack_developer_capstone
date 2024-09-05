946c47088480c855832d92de6b7c0a8700fa3852
devliet <101347493+devliet@users.noreply.github.com>
2024-09-05 15:55:31 +0200

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,13 +30,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
                     'localhost',
-                     "".join(
+                    "".join(
                          [
                             'https://etiennedevl1-8000',
                             '.theiadockernext-0-labs-prod-theiak8s-4-tor01.',
                             'proxy.cognitiveclass.ai'
                          ]
-                     )
+                    )
 ]
 CSRF_TRUSTED_ORIGINS = [
     "".join(
